b7350a0e683638054aace9a01cc6367a9ca12e0b
Sergey Korchagin <millagros77@gmail.com>
2023-07-14 15:49:59 +0300

    menu button added

--- src/main/java/com/example/SpringQRBot/service/TelegramBot.java
+++ src/main/java/com/example/SpringQRBot/service/TelegramBot.java
@@ -4,15 +4,35 @@ import com.example.SpringQRBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScope;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiValidationException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     BotConfig config;
     public TelegramBot(BotConfig config){
         this.config = config;
+        List<BotCommand> commands = new ArrayList<>();
+        commands.add(new BotCommand("/start", "get a welcome message"));
+        commands.add(new BotCommand("/mydata", "get your data stored"));
+        commands.add(new BotCommand("/deletedata", "delete my data"));
+        commands.add(new BotCommand("/help", "info how to use this bot"));
+        commands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            execute(new SetMyCommands(commands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
     }
 
     @Override
